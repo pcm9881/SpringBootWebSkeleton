be2b9f7872a85d87bb56c6a33d56592e9a99d52c
Chungman Park <chungmanpark@Chungmanui-MacBookPro.local>
2023-05-15 13:35:35 +0900

    웹 프로젝트 STEP 1

---
+++ src/main/java/com/pcm9881/springbootwebskeleton/SpringBootWebSkeletonApplication.java
@@ -0,0 +1,13 @@
+package com.pcm9881.springbootwebskeleton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebSkeletonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWebSkeletonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pcm9881/springbootwebskeleton/SpringBootWebSkeletonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pcm9881.springbootwebskeleton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebSkeletonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
